8f68ce00ebfc703be4ad8fae75ca09d49ffbeec3
asebastian36 <asebastianjob@gmail.com>
2023-10-15 18:51:59 -0600

    creacion de un nuevo proyecto que abarcara inyeccion de dependencias

--- spring-boot-web/src/main/java/com/bolsadeideas/springboot/web/app/controllers/HomeController.java
+++ spring-boot-web/src/main/java/com/bolsadeideas/springboot/web/app/controllers/HomeController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
-    @GetMapping("/")
+    @GetMapping(value = { "/", "" })
     public String home() {
         return "forward:/app/index";
     }
