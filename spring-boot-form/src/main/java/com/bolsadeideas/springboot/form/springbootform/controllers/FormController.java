274048a7a1cd43d21cbecaac3f6d559418d56426
asebastian36 <asebastianjob@gmail.com>
2023-12-10 13:58:39 -0600

    avances con el proyecto de formulario

--- spring-boot-form/src/main/java/com/bolsadeideas/springboot/form/springbootform/controllers/FormController.java
+++ spring-boot-form/src/main/java/com/bolsadeideas/springboot/form/springbootform/controllers/FormController.java
@@ -1,19 +1,32 @@
 package com.bolsadeideas.springboot.form.springbootform.controllers;
 
+import com.bolsadeideas.springboot.form.springbootform.models.domain.Usuario;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class FormController {
     @GetMapping("/form")
     public String form(Model model) {
+        model.addAttribute("titulo", "Formulario");
         return "form";
     }
 
     @PostMapping("/form")
-    public String procesar(Model model) {
+    public String procesar(@RequestParam String username,
+                           @RequestParam String email,
+                           @RequestParam String password,
+                           Model model) {
+
+        Usuario user = new Usuario();
+
+        user.setEmail(email);
+        user.setUsername(username);
+        user.setPassword(password);
+
+        model.addAttribute("titulo", "Datos procesados");
+        model.addAttribute("user", user);
         return "resultado";
     }
 }
