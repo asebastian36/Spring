a329a4714beee68553357460f5a0a4a20b47f183
asebastian36 <asebastianjob@gmail.com>
2023-12-18 21:31:57 -0600

    primer proyecto

---
+++ webapp/src/test/java/com/angel/curso/springboot/webapp/WebappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.angel.curso.springboot.webapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebappApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
