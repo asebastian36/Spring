1e3328bfa438aae208eaa91c30bbff67fa3d1f5d
asebastian36 <asebastianjob@gmail.com>
2024-02-19 11:00:59 -0600

    avances en el proyecto de jpa

--- springboot-jpa/src/main/java/com/angel/curso/springboot/springbootjpa/SpringbootJpaApplication.java
+++ springboot-jpa/src/main/java/com/angel/curso/springboot/springbootjpa/SpringbootJpaApplication.java
@@ -7,10 +7,7 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.transaction.annotation.Transactional;
-
-import java.util.List;
-import java.util.Optional;
-import java.util.Scanner;
+import java.util.*;
 
 @SpringBootApplication
 public class SpringbootJpaApplication implements CommandLineRunner {
@@ -24,9 +21,64 @@ public class SpringbootJpaApplication implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-        terminal();
+        personalizedQueries(1L);
     }
 
+    @Transactional(readOnly = true)
+    public void personalizedQueries(Long id) {
+        String result = repository.getNameById(id);
+        System.out.println("result = " + result);
+        
+        String fullName = repository.getFullNameById(id);
+        System.out.println("fullName = " + fullName);
+        
+        Object[] array = repository.getFullById(id);
+        System.out.println(array[0].toString());
+    }
+
+    @Transactional
+    public void delete() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Ingrese el id del registro a eliminar: ");
+        Long id = sc.nextLong();
+
+        Optional<Person> person = repository.findById(id);
+
+        person.ifPresentOrElse(repository::delete, () -> System.out.println("Registro no encontrado"));
+    }
+
+    @Transactional
+    public void  update() {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Ingrese el id del registro a actualizar: ");
+        Long id = sc.nextLong();
+
+        Optional<Person> person = repository.findById(id);
+
+        person.ifPresent(p -> {
+            System.out.println("Registro encontrado: " + p);
+
+            System.out.println("Ingrese el lenguaje de programacion: ");
+            String lenguaje = sc.next();
+
+            System.out.println("Ingrese el nombre: ");
+            String nombre = sc.next();
+
+            System.out.println("Ingrese el apellido: ");
+            String apellido = sc.next();
+
+            p.setName(nombre);
+            p.setLastname(apellido);
+            p.setProgrammingLanguage(lenguaje);
+
+            repository.save(p);
+
+            System.out.println("Registro actualizado: " + p);
+        });
+    }
+
+    @Transactional
     public void terminal() {
         Scanner sc = new Scanner(System.in);
         String name = sc.next();
@@ -46,7 +98,7 @@ public class SpringbootJpaApplication implements CommandLineRunner {
         repository.save(person);
     }
 
-
+    @Transactional(readOnly = true)
     public void findOne() {
         //  opcion 1
 //        Person person = null;
@@ -60,6 +112,7 @@ public class SpringbootJpaApplication implements CommandLineRunner {
         repository.findOneNameLike("gel").ifPresent(System.out::println);
     }
 
+    @Transactional(readOnly = true)
     public void list() {
         //  List<Person> persons = (List<Person>) repository.findByIdAndName(1, "Angel");
         //  persons.forEach(System.out::println);
--- springboot-jpa/src/main/java/com/angel/curso/springboot/springbootjpa/repositories/PersonRepository.java
+++ springboot-jpa/src/main/java/com/angel/curso/springboot/springbootjpa/repositories/PersonRepository.java
@@ -19,9 +19,10 @@ public interface PersonRepository extends CrudRepository<Person, Long> {
     //  personalizado y automatico
     List<Person> findByIdAndName(long id, String name);
 
-    //  personalizadas que retornan un arreglo
+    //  personalizadas que retornan un arreglo, no una entidad
     @Query("select p.name, p.lastname, p.programmingLanguage from Person p")
     List<Object[]> obtenerPersonValues();
+
 
     @Query("select p.name, p.lastname, p.programmingLanguage from Person p where p.name = ?1 and p.lastname = ?2 and p.programmingLanguage = ?3")
     List<Object[]> obtenerPersonValues(String name, String lastname, String programmingLanguage);
@@ -39,7 +40,18 @@ public interface PersonRepository extends CrudRepository<Person, Long> {
     Optional<Person> findOneName(String name);
 
     //  para buscar por contenido aunque no sea exactamente igual
-    @Query("select p from Person p where p.name like  %?1%")//  % al inicio que empice con
-    //   % al final que termine con, dos % que contenga
+    @Query("select p from Person p where p.name like  %?1%")//  % al inicio que empice con % al final que termine con, dos % que contenga
     Optional<Person> findOneNameLike(String name);
+
+    //  para retornar el campo
+    @Query("select p.name from Person p where p.id = :id")
+    String getNameById(Long id);
+
+    //  obtener nombre y apellido
+    @Query("select concat(p.name, ' ' , p.lastname) as fullname from Person p where p.id = :id")
+    String getFullNameById(Long id);
+
+    //  obtener todos los campos de un registro
+    @Query("select p.id, p.name, p.lastname, p.programmingLanguage from Person p where p.id = :id")
+    Object[] getFullById(Long id);
 }
